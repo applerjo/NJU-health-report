3a3b2f5eaa29aa8ec983b3416faeb546236edc1c
Roc <1361050885@qq.com>
2022-04-18 02:05:07 +0800

    优化打卡信息填写方式（地址和核酸检测时间）、添加邮箱提醒

--- run.py
+++ run.py
@@ -6,18 +6,47 @@ import time
 import logging
 import datetime
 from pytz import timezone
-from njupass.ocr import detect
 
 URL_JKDK_LIST = 'http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/getApplyInfoList.do'
 URL_JKDK_APPLY = 'http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/saveApplyInfos.do'
 
 auth = NjuUiaAuth()
 
-def get_zjhs_time(method='YESTERDAY'):
+
+def notify(msg):
+    with open('email.txt', 'a+') as f:
+        f.write(msg + '\n')
+    return
+
+
+def get_normalization_date(username):
+    today = datetime.datetime.now(timezone('Asia/Shanghai'))
+    date = datetime.datetime(2022, 4, 4 + (int(username[-1]) + 4) % 5, 8, 30)
+    while (today.replace(tzinfo=None) - date).days >= 5:
+        date += datetime.timedelta(days=5)
+    return date
+
+
+def get_zjhs_time(method='YESTERDAY', username=None, last_time=None):
     today = datetime.datetime.now(timezone('Asia/Shanghai'))
     yesterday = today + datetime.timedelta(-1)
     if method == 'YESTERDAY':
         return yesterday.strftime("%Y-%m-%d %-H")
+    elif method == 'LAST':
+        return last_time
+    elif method == 'NORMALIZATION':
+        return get_normalization_date(username).strftime("%Y-%m-%d %-H")
+    elif method == 'NORMALIZATION&LAST':
+        if get_normalization_date(username) < datetime.datetime(
+                int(last_time[:4]),
+                int(last_time[5:7]),
+                int(last_time[8:10]),
+                int(last_time[11:13])
+        ):
+            date = last_time
+        else:
+            date = get_normalization_date(username).strftime("%Y-%m-%d %-H")
+        return date
 
 
 if __name__ == "__main__":
@@ -28,14 +57,19 @@ if __name__ == "__main__":
 
     username = os.getenv('NJU_USERNAME')
     password = os.getenv('NJU_PASSWORD')
-    curr_location = os.getenv('CURR_LOCATION')
+    location_info_from = os.getenv('LOCATION_INFO_FROM')
     method = os.getenv('COVID_TEST_METHOD')
+    curr_location = ''
+    zjhs_time = ''
 
+    if location_info_from == '':
+        location_info_from = 'LAST'
     if method == '':
         method = 'YESTERDAY'
 
-    if username == '' or password == '' or curr_location == '':
+    if username == '' or password == '' or (location_info_from == 'CONFIG' and curr_location == ''):
         log.error('账户、密码或地理位置信息为空！请检查是否正确地设置了 SECRET 项（GitHub Action）。')
+        notify('账户、密码或地理位置信息为空！请检查是否正确地设置了 SECRET 项（GitHub Action）。')
         os._exit(1)
 
     log.info('尝试登录...')
@@ -46,6 +80,7 @@ if __name__ == "__main__":
     ok = auth.tryLogin(username, password)
     if not ok:
         log.error("登录失败。可能是用户名或密码错误，或是验证码无法识别。")
+        notify("登录失败。可能是用户名或密码错误，或是验证码无法识别。")
         os._exit(1)
 
     log.info('登录成功！')
@@ -57,19 +92,28 @@ if __name__ == "__main__":
             log.error('获取失败，一分钟后再次尝试...')
             time.sleep(60)
             continue
+        dk_info = json.loads(r.text)['data']
 
-        dk_info = json.loads(r.text)['data'][0]
-        if dk_info['TBZT'] == "0":
-            wid = dk_info['WID']
+        # 根据配置填写地址和核酸检测信息
+        if location_info_from == 'CONFIG':
+            curr_location = os.getenv('CURR_LOCATION')
+        else:
+            curr_location = dk_info[1]["CURR_LOCATION"]
+        zjhs_time = get_zjhs_time(method, username, dk_info[1]['ZJHSJCSJ'])
+
+        if dk_info[0]['TBZT'] == "0":
+            wid = dk_info[0]['WID']
             data = "?WID={}&IS_TWZC=1&CURR_LOCATION={}&ZJHSJCSJ={}&JRSKMYS=1&IS_HAS_JKQK=1&JZRJRSKMYS=1&SFZJLN=0".format(
-                wid, curr_location, get_zjhs_time())
+                wid, curr_location, zjhs_time)
             url = URL_JKDK_APPLY + data
             log.info('正在打卡')
             auth.session.get(url)
             time.sleep(1)
         else:
-            log.info("今日已打卡！")
+            log.info("打卡成功！")
+            notify("打卡成功！")
             os._exit(0)
 
     log.error("打卡失败，请尝试手动打卡")
+    notify("打卡失败，请手动打卡！")
     os._exit(-1)
